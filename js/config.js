1615feeb3c1b80cc25dd08f8083d2ddd7938a8df
chessyokjvl <qualityjvl@gmail.com>
2026-03-23 16:31:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,2 @@
-// js/config.js
-const CONFIG = {
-    // *** ใส่ URL Web App ของคุณที่นี่ที่เดียว ***
-    API_URL: 'https://script.google.com/macros/s/AKfycbza4EAFjZbKSpvUx_xoL9YRSnLWetFVBMEsIJVdpR_sgJtNhotvOiOms7T5YZAlDzGpgA/exec' 
-};
+// ไฟล์ config.js
+const API_URL = 'https://script.google.com/macros/s/AKfycbza4EAFjZbKSpvUx_xoL9YRSnLWetFVBMEsIJVdpR_sgJtNhotvOiOms7T5YZAlDzGpgA/exec';
