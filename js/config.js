3af4a6e13bb46cbe4f8c7e7325f58aae38c530c5
chessyokjvl <qualityjvl@gmail.com>
2026-03-23 16:38:39 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-// ไฟล์ config.js
-const API_URL = 'https://script.google.com/macros/s/AKfycbza4EAFjZbKSpvUx_xoL9YRSnLWetFVBMEsIJVdpR_sgJtNhotvOiOms7T5YZAlDzGpgA/exec';
+// URL ของ Google Apps Script ที่ Deploy เสร็จแล้ว
+const API_URL = 'https://script.google.com/macros/s/AKfycbxNJ5l0eHilYZyDgSgqp_MPEKWVKZfO672LLu8nkERZKxEsJ81lBDf5jZY58wVDMD2B5Q/exec';
