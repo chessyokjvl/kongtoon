b31ed3d5da1ad2224706ccb4c1272069ba2e0fbf
chessyokjvl <qualityjvl@gmail.com>
2026-03-23 13:51:47 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// js/config.js
+const CONFIG = {
+    // *** ใส่ URL Web App ของคุณที่นี่ที่เดียว ***
+    API_URL: 'https://script.google.com/macros/s/AKfycbza4EAFjZbKSpvUx_xoL9YRSnLWetFVBMEsIJVdpR_sgJtNhotvOiOms7T5YZAlDzGpgA/exec' 
+};
